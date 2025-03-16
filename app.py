2e11feea89c3f348c80217147e49a991e312f089
Sean Shiroma <111812174+sean-ks@users.noreply.github.com>
2025-03-16 01:30:51 -0400

    fx

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # Add this line
+from flask_cors import CORS 
 import os
 import json
 from services.fact_checker import FactChecker
 
 app = Flask(__name__)
-CORS(app)  # Add this line to enable CORS
+CORS(app)
 fact_checker = FactChecker()
 
 @app.route('/')
